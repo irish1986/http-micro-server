9165d01c301ec0a7c688999ae541fda7c366636c
Simon HARVEY <simon.harvey.86@gmail.com>
2024-01-18 11:35:38 -0500

    Repo Init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # http-micro-server
+
+A simple http server simulating a micro-serverice architecture from scratch.
+
+## Getting Started
---
+++ app/main.py
@@ -0,0 +1,39 @@
+import os.path
+from time import sleep
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import contextlib
+
+HOST = "0.0.0.0"
+PORT = 8080
+PATH = "/etc/message.txt"
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+
+        message = "Hello World!"
+
+        if os.path.isfile(PATH):
+            with open(PATH, "r") as f:
+                message = f.read()
+
+        self.wfile.write(bytes(message, "utf-8"))
+
+
+def run(host, port, server_class=HTTPServer, handler_class=Handler):
+    server_address = (host, port)
+    httpd = server_class(server_address, handler_class)
+
+    with contextlib.suppress(KeyboardInterrupt):
+        httpd.serve_forever()
+
+    httpd.server_close()
+
+
+if __name__ == "__main__":
+    sleep(30)
+    print("Starting server...")
+    run(HOST, PORT)
